ad898c90850764ca58f502bcfe7035f29427e053
Redblood639 <albinorabbithunter@gmail.com>
2022-07-23 06:34:19 +0900

    feat : audio and logo

---
+++ src/custom-types.d.ts
@@ -0,0 +1,10 @@
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.svg";
+declare module "*.gif";
+declare module "*.webp";
+declare module "*.mp3";
+declare module "stylis-plugin-rtl" {
+  const noTypesYet: any;
+  export default noTypesYet;
+}
